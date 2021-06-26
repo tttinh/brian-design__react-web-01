bfe6eb19a108868dd55a68cd37930e499198a5e2
tinhtt-tokoin <tinh.tran@tokoin.io>
2021-06-26 17:26:55 +0700

    Implement Navbar.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import './App.css';
+import React from 'react';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
