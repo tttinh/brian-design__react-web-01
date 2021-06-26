72fe6fbcbd7338088eeb700e79418125b176e4f8
tinhtt-tokoin <tinh.tran@tokoin.io>
2021-06-27 01:04:16 +0700

    Add more pages.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import Navbar from './components/Navbar';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/pages/Home.js';
+import Services from './components/pages/Services.js';
+import Products from './components/pages/Products.js';
+import SignUp from './components/pages/SignUp.js';
+
 
 function App() {
   return (
@@ -11,6 +15,9 @@ function App() {
         <Navbar />
         <Switch>
           <Route path='/' exact component={Home} />
+          <Route path='/services' exact component={Services} />
+          <Route path='/products' exact component={Products} />
+          <Route path='/sign-up' exact component={SignUp} />
         </Switch>
       </Router>
     </>
