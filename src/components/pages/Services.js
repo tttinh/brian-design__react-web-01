124d0004b8f987fa61091bbff54fa532d7ed9208
tinhtt-tokoin <tinh.tran@tokoin.io>
2021-06-27 14:28:47 +0700

    Add footer component.

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import '../../App.css'
+import Footer from '../Footer';
 
 function Services() {
-  return <h1 className='services'>SERVICES</h1>;
+  return (
+    <>
+      <h1 className='services'>SERVICES</h1>
+      <Footer />
+    </>
+  );
 }
 
 export default Services;
--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import '../../App.css'
+import Footer from '../Footer';
 
 function SignUp() {
-  return <h1 className='sign-up'>SIGN UP</h1>;
+  return (
+    <>
+      <h1 className='sign-up'>SIGN UP</h1>
+      <Footer />
+    </>
+  );
 }
 
 export default SignUp;
